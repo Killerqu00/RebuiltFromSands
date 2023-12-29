516cdad4a1ed9aa9c25baeb8d26da59a2205aa0d
Killerqu00 <killerqueen1777@gmail.com>
2023-12-29 11:38:04 +0100

    alpha1

--- kubejs/server_scripts/recipes/mekanism.js
+++ kubejs/server_scripts/recipes/mekanism.js
@@ -9,4 +9,19 @@ ServerEvents.recipes(event => {
             '#forge:dusts/redstone'
         ]
     )
+    event.shaped(
+        Item.of('custommachinery:custom_machine_item', '{machine:"custommachines:sand_drill"}'),
+        [
+            'PCP',
+            'RFR',
+            'PDP'
+        ],
+        {
+            P: '#forge:plates/steel',
+            C: 'mekanism:basic_control_circuit',
+            R: '#forge:dusts/redstone',
+            F: 'mekanism:steel_casing',
+            D: 'immersiveengineering:drillhead_steel'
+        }
+    )
 })
